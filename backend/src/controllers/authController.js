d02c2249bbb74c263e7aea17298b133113b99e01
ZekiKobe <kobezekibelay@gmail.com>
2026-06-20 22:26:36 +0300

    Refactor user registration response structure and error handling

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,29 +4,36 @@ const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
-    const { firstName, lastName, email, password } = req.body;
+    const existingUser = await User.findOne({
+      email: req.body.email,
+    });
 
-    const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
     const newUser = new User({
-      firstName,
-      lastName,
-      email,
+      ...req.body,
       password: hashedPassword,
     });
+
     await newUser.save();
+
     res.status(201).json({
-         success: true,
-         data:newUser,
-         message: "User registered successfully" 
-        });
+      success: true,
+      data: newUser,
+      message: "User registered successfully",
+    });
   } catch (error) {
-    console.log("Error Registering user", error);
-    res.status(500).json({ message: error.message });
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
