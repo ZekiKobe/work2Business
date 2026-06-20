3e053df4fe13fb858c2d37015f5b8ebcef4897cd
ZekiKobe <kobezekibelay@gmail.com>
2026-06-21 01:45:52 +0300

    feat: implement authentication context with login and logout functionality

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
+    const [ user, setUser ] = useState(null);
+
+    const login = (data) => {
+        localStorage.setItem("token", data.token);
+        setUser(data.user);
+    };
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        setUser(null);
+    };
+
+    return <AuthContext.Provider value={{ user, login, logout}}>
+        {children}
+    </AuthContext.Provider>
+}
